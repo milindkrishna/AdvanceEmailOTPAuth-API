470059e9007b7fcf262c645588e3981da39ca1ae
milindkrishna <54195987+milindkrishna@users.noreply.github.com>
2024-09-17 15:32:01 +0530

    added swagger api  v1.0

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,11 +3,179 @@ import { login, signup, logout, verifyemail, forgotpassword, resetpassword, chec
 import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * /api/auth/check-auth:
+ *  get:
+ *    summary: Check if user is authenticated
+ *    tags: [Authorization]
+ *    responses:
+ *      200:
+ *        description: User is authenticated
+ */
 router.route('/check-auth').get(verifyToken, checkAuth)
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *  post:
+ *    summary: Login user
+ *    tags: [Login User]
+ *    parameters:
+ *      - in: body
+ *        name: user
+ *        description: Login request body
+ *        schema:
+ *          type: object
+ *          required:
+ *            - email
+ *            - password
+ *          properties:
+ *            email:
+ *              type: string
+ *              description: User email
+ *              example: johndoe@example.com
+ *            password:
+ *              type: string
+ *              description: User password
+ *              example: password123
+ *    responses:
+ *      200:
+ *        description: User logged in
+ */
 router.route('/login').post(login)
+
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *  post:
+ *    summary: Register user
+ *    tags: [Signup/Regsiter User]
+ *    parameters:
+ *      - in: body
+ *        name: user
+ *        description: User registration details
+ *        schema:
+ *          type: object
+ *          required:
+ *            - name
+ *            - email
+ *            - password
+ *          properties:
+ *            name:
+ *              type: string
+ *              description: User name
+ *              example: John Doe
+ *            email:
+ *              type: string
+ *              description: User email
+ *              example: johndoe@example.com
+ *            password:
+ *              type: string
+ *              description: User password
+ *              example: password123
+
+ *    responses:
+ *      200:
+ *        description: User registered
+ */
+
 router.route('/register').post(signup)
+
+/**
+ * @swagger
+ * /api/auth/logout:
+ *  post:
+ *    summary: Logout user
+ *    tags: [Logout User]
+ *    responses:
+ *      200:
+ *        description: User logged out
+ */
 router.route('/logout').post(logout)
+
+
+/**
+ * @swagger
+ * /api/auth/verify-email:
+ *  post:
+ *    summary: Verify email
+ *    tags: [Verify User Email]
+ *    parameters:
+ *      - in: body
+ *        name: user
+ *        description: Verify email request body
+ *        schema:
+ *          type: object
+ *          required:
+ *            - code
+ *          properties:
+ *            code:
+ *               type: string
+ *               description: Verification otp
+ *               example: abc123
+ *    responses:
+ *      200:
+ *        description: Email verified
+ */
+
 router.route('/verify-email').post(verifyemail)
+
+/**
+ * @swagger
+ * /api/auth/forgot-password:
+ *  post:
+ *    summary: Forgot password
+ *    tags: [Forgot User Password]
+ *    parameters:
+ *      - in: body
+ *        name: user
+ *        description: Forgot password request body
+ *        schema:
+ *          type: object
+ *          required:
+ *            - email
+ *          properties:
+ *             email:
+ *               type: string
+ *               description: User email
+ *               example: johndoe@example.com
+ *    responses:
+ *      200:
+ *        description: Password reset
+ */
 router.route('/forgot-password').post(forgotpassword)
+
+/**
+ * @swagger
+ * /api/auth/reset-password/{token}:
+ *  post:
+ *    summary: Reset password
+ *    tags: [Reset User Password]
+ *    parameters:
+ *      - in: path
+ *        name: token
+ *        required: true
+ *        description: Password reset token
+ *      - in: body
+ *        name: user
+ *        description: Reset password request body
+ *        schema:
+ *           type: object
+ *           required:
+ *             - password
+ *           properties:
+ *             password:
+ *               type: string
+ *               description: New password
+ *               example: password123
+ *    responses:
+ *      200:
+ *        description: Password reset
+ */
 router.route('/reset-password/:token').post(resetpassword)
+
+
 export default router
--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,61 @@ import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 5000;
 import { connectDB } from "./db/db.config.js";
 import authRoutes from "./routes/auth.route.js";
-
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerui from "swagger-ui-express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 app.use(express.json())
 app.use(cookieParser()) // allow to parse incoming cookies
+app.use(express.static('public'));
 
+
+// Swagger options
+const swaggeroptions = {
+    definition: {
+     openapi: "3.1.0",
+      info: {
+        title: "Auth API",
+        version: "1.0.0",
+        description: "Auth API Information using Nodejs and Swagger UI \n Build with ♥ by Milind"
+      },
+      consumes: ['application/json'],
+      servers: [
+        {
+          url: `http://localhost:${PORT}`,
+          description: "Local Development Server"
+        },
+        // {
+        //   url: `http://localhost:5000`,
+        //   description: "Prod Server"
+        // }
+      ],
+    },
+    apis: ["./backend/routes/*.js"]
+  }
+  
+
+const swaggerSpecs = swaggerJsdoc(swaggeroptions)
+
+// swagger is available at http://localhost:${PORT}/api-docs
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpecs))
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Routes
 app.use('/api/auth', authRoutes)
+
+
+// Wildcard route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 
 
 app.listen(PORT, () => {
